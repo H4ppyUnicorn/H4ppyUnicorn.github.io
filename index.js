adb0306e723216809fc492831df359e08b0ef0ec
HappyUnicorn <buiiibeckuy@gmail.com>
2024-04-03 21:50:05 +0500

    chefs added, some other stuff idk

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Я ненавижу JavaScript. © HappyUnicorn
 let main_div = document.querySelector(".main")
+let content_div = document.querySelector(".content")
+content_div.style.display = "none"
 setTimeout(function(){
     main_div.style.display = "none"
-}, "3000")
+}, "2000")
+setTimeout(function(){
+    content_div.style.display = "block"
+}, "2000")
